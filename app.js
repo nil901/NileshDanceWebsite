760674fa0f66f4a10eb5f3c45625ef1d38c332f4
email <pathaknilesh1998@gmail.com>
2022-01-23 09:03:36 +0530

    app file updated

--- app.js
+++ app.js
@@ -2,10 +2,20 @@ const express = require("express");
 const path = require("path");
 const fs = require('fs')
 // getting-started.js
+var mongoose = require('mongoose') ;
+mongoose.connect('mongodb://localhost/conntactDance',{useNewUrlParser:true} );
 const app = express();
+
 const port = 80;
 
-
+//difine mongoode schema
+const contactSchema = new mongoose.Schema({
+    name: String,
+    phone: String,
+    address: String,
+    email: String,
+    desc: String
+  });
 
 // app.use(express.static('static', options)) 
 
@@ -36,15 +46,17 @@ app.get('/Services', (req, res)=>{
     res.status(200).render('Services.pug', params);
 })
 
-app.get('/contact', (req, res)=>{
-    
-    const params = {}
-    res.status(200).render('contact.pug', params);
-})
+app.post('/contact', (req, res)=>{
+    var myData = new Contact(req.body);
+    myData.save().then(()=>{
+    res.send("This item has been saved to the database")
+    }).catch(()=>{
+    res.status(400).send("item was not saved to the databse")
+});
 
 
 
 // START THE SERVER
 app.listen(port, ()=>{
     console.log(`The application started successfully on port ${port}`);
-});
+})
